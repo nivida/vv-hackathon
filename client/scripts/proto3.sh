953944b20b6ad5ee06699c069733c4eb320d2bee
nivida <samuel@ethereum.org>
2020-04-03 21:49:51 +0200

    build:proto updated

--- client/scripts/proto3.sh
+++ client/scripts/proto3.sh
@@ -1,2 +1,11 @@
 
-protoc --proto_path=../proto OES.proto --js_out=import_style=commonjs:./proto-clients
+find ../proto/ -type f -name *.proto -exec \
+		protoc \
+         --proto_path=../proto \
+         --js_out=import_style=commonjs:./proto-clients \
+    {} ';'   \
+    -exec \
+		protoc \
+         --proto_path=../proto \
+        --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./proto-clients \
+    {} ';'   \
