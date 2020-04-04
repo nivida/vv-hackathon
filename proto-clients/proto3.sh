1f925ae49cb49e2c93edc425c47a25ce4cdb9c67
Shane Hofstetter <shane.hofstetter@gmail.com>
2020-04-04 11:50:22 +0200

    fix proto client build script

--- proto-clients/proto3.sh
+++ proto-clients/proto3.sh
@@ -2,10 +2,10 @@
 find ../proto/ -type f -name *.proto -exec \
 		protoc \
          --proto_path=../ \
-         --js_out=import_style=commonjs:./proto \
+         --js_out=import_style=commonjs:./ \
     {} ';'   \
     -exec \
 		protoc \
          --proto_path=../ \
-        --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./proto \
+        --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./ \
     {} ';'   \
