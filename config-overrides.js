6722ea153033b8f357e457840b3d142d421d0729
nivida <samuel@ethereum.org>
2020-04-05 08:22:51 +0200

    school logo from Aarau added to header

--- config-overrides.js
+++ config-overrides.js
@@ -9,5 +9,5 @@ module.exports = override(
     addLessLoader({
         javascriptEnabled: true,
         modifyVars: {'@primary-color': '#5901b8'},
-    }),
+    })
 );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,8 @@
-import {Layout} from "antd";
+import {Avatar, Layout} from "antd";
 import React from "react";
 import Account from "./Account";
 import "./header.scss"
+import logo from "../../assets/images/logo-schule-aarau.png";
 
 const Header = (props) => {
   const {
@@ -10,9 +11,9 @@ const Header = (props) => {
   } = props;
 
   return <Layout.Header style={{background: '#fff'}}>
-    {/*<div className={'left-content'}>*/}
-    {/*  <Brand collapsed={isMobile}/>*/}
-    {/*</div>*/}
+    <div className={'left-content'}  style={{float: 'left'}}>
+      <img src={logo} style={{width: '150px'}}/>
+    </div>
     {children}
     <div className={'right-content'} style={{float: 'right'}}>
       <Account/>
