0ced890053047e0ae901198e78e2f23180d4e0d1
Shane Hofstetter <shane.hofstetter@gmail.com>
2020-04-05 04:24:52 +0200

    change timeline style

--- src/components/students/timeline/LessonItem.js
+++ src/components/students/timeline/LessonItem.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import {useState} from "react";
-import {Button, Card, Col, Row} from "antd";
+import {Button, Col, Row, Tag} from "antd";
 import LessonDetails from "./LessonDetails";
 import {DownOutlined, UpOutlined} from "@ant-design/icons";
 import moment from "moment";
@@ -11,34 +11,42 @@ const LessonItem = ({lesson, ...props}) => {
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <Card title={false} bordered={false} bodyStyle={{padding: 25, paddingBottom: 5}}>
-      <Row justify={'space-between'} type={'flex'}>
-        <Col>
-          <p style={{
-            fontWeight: 'bold',
-            margin: 0
-          }}>{`${moment.unix(lesson.start).format(timeFormat)} - ${moment.unix(lesson.end).format(timeFormat)}`}</p>
-          <h2 style={{margin: 0}}>{lesson.title}</h2>
-        </Col>
-        {expanded ? (
-          <Col span={24}>
-            <LessonDetails style={{marginTop: 20}} lesson={lesson}/>
-          </Col>
+    // <Card title={false} bordered={false} bodyStyle={{padding: 25, paddingBottom: 5}}>
+    <Row justify={'space-between'} type={'flex'}>
+      <Col>
+        <p style={{
+          fontWeight: 'bold',
+          margin: 0,
+          fontSize: 14
+        }}>{`${moment.unix(lesson.start).format(timeFormat)} - ${moment.unix(lesson.end).format(timeFormat)}`}</p>
+        <h2 style={{margin: 0}}>{lesson.title}</h2>
+      </Col>
+      <Col>
+        {lesson.tags ? (
+          lesson.tags.map((tag, i) => (
+            <Tag key={tag} color={["purple", "magenta", "green"][i]}>{tag}</Tag>
+          ))
         ) : null}
+      </Col>
+      {expanded ? (
         <Col span={24}>
-          <Row justify={'center'} type={'flex'}>
-            <Button onClick={() => setExpanded(expanded => !expanded)} type={'link'}
-                    style={{marginTop: expanded ? 20 : 0}}>
-              {expanded ? (
-                <><UpOutlined/> Less</>
-              ) : (
-                <><DownOutlined/> Details</>
-              )}
-            </Button>
-          </Row>
+          <LessonDetails style={{marginTop: 20}} lesson={lesson}/>
         </Col>
-      </Row>
-    </Card>
+      ) : null}
+      <Col span={24}>
+        <Row justify={'center'} type={'flex'}>
+          <Button onClick={() => setExpanded(expanded => !expanded)} type={'link'}
+                  style={{marginTop: expanded ? 20 : 0}}>
+            {expanded ? (
+              <><UpOutlined/> Less</>
+            ) : (
+              <><DownOutlined/> Details</>
+            )}
+          </Button>
+        </Row>
+      </Col>
+    </Row>
+    // </Card>
   )
 };
 
--- src/components/students/timeline/Lessons.js
+++ src/components/students/timeline/Lessons.js
@@ -1,24 +1,50 @@
 import * as React from "react";
+import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import {BookOutlined} from "@ant-design/icons";
 import LessonItem from "./LessonItem";
-import {ConfigProvider, Empty, List} from "antd";
+// import * as moment from "moment";
+// import {timeFormat} from "../../../utils/dateFormats";
 
 const Lessons = ({lessons = []}) => {
 
+  const defaultElementProps = {
+    iconStyle: {background: '#fff', color: '#5901b8'},
+    icon: <BookOutlined/>,
+    className: "vertical-timeline-element--work",
+  };
+
   return (
-    <ConfigProvider renderEmpty={() => (
-      <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={'No lessons'}/>
-    )}>
-      <List
-        grid={{gutter: 16, xs: 1, sm: 1, md: 1, lg: 1}}
-        dataSource={lessons}
-        renderItem={lesson => (
-          <List.Item key={lesson.id}>
-            <LessonItem lesson={lesson}/>
-          </List.Item>
-        )}
-      />
-    </ConfigProvider>
+    <VerticalTimeline layout={'1-column'}>
+      {lessons.map(lesson => (
+        <VerticalTimelineElement
+          key={lesson.id}
+          // date={`${moment.unix(lesson.start).format(timeFormat)} - ${moment.unix(lesson.end).format(timeFormat)}`}
+          {...defaultElementProps}
+        >
+          {/*<h3 className="vertical-timeline-element-title">{lesson.title}</h3>*/}
+          {/*<h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>*/}
+          <LessonItem lesson={lesson}/>
+        </VerticalTimelineElement>
+      ))}
+    </VerticalTimeline>
   );
+
+  // return (
+  //   <ConfigProvider renderEmpty={() => (
+  //     <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={'No lessons'}/>
+  //   )}>
+  //     <List
+  //       grid={{gutter: 16, xs: 1, sm: 1, md: 1, lg: 1}}
+  //       dataSource={lessons}
+  //       renderItem={lesson => (
+  //         <List.Item key={lesson.id}>
+  //           <LessonItem lesson={lesson}/>
+  //         </List.Item>
+  //       )}
+  //     />
+  //   </ConfigProvider>
+  // );
 };
 
 export default Lessons;
