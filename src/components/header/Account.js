a7343d457709b122d4a2c99e8b14a0ea396e5818
nivida <samuel@ethereum.org>
2020-04-05 04:00:58 +0200

    header updated

--- src/components/header/Account.js
+++ src/components/header/Account.js
@@ -1,14 +1,25 @@
-import {Button, Dropdown, Menu} from "antd";
+import {Avatar, Button, Dropdown, Menu, Progress, Tooltip} from "antd";
 import * as React from "react";
 import {useContext} from "react";
 import {LogoutOutlined} from "@ant-design/icons";
 import {StoreContext} from "../../repositories/rootRepo";
 import {observer} from "mobx-react-lite";
 import SettingFilled from "@ant-design/icons/es/icons/SettingFilled";
+import avatar from "../../assets/images/avatar.png";
+import {useState} from "react";
+import {useEffect} from "react";
 
 const Account = (props) => {
 
   const store = useContext(StoreContext);
+  const [name, setName] = useState(null);
+  const [credits, setCredits] = useState(null);
+
+  useEffect(() => {
+    setName(store.authRepo.user.name);
+    setCredits(store.authRepo.user.credits);
+  }, []);
+
 
   const logout = () => {
     store.authRepo.logout().then(() => {
@@ -18,11 +29,11 @@ const Account = (props) => {
 
   const menu = (
     <Menu className={'user-menu'} selectedKeys={[]}>
-      <Menu.Item key="privacy">
-        Privacy
+      <Menu.Item key="settings">
+        <SettingFilled/> Settings
       </Menu.Item>
-      <Menu.Item key="feedback">
-        Feedback
+      <Menu.Item key="logout" onClick={logout}>
+        <LogoutOutlined/> Logout
       </Menu.Item>
     </Menu>
   );
@@ -30,13 +41,21 @@ const Account = (props) => {
   return (
     <div>
       <Dropdown overlay={menu} trigger={['click']}>
-        <Button>
-          <SettingFilled />
-        </Button>
+        <Avatar
+          size={"large"}
+          className={'user-avatar'}
+          alt="avatar"
+          src={avatar}
+        />
       </Dropdown>
-      <Button onClick={logout} style={{marginLeft: '10px'}}>
-        <LogoutOutlined />
-      </Button>
+      <Tooltip title={'Credits: ' + credits}>
+        <span style={{paddingLeft: '10px'}}>
+          {name}
+        </span>
+        <div style={{height: '20px', marginTop: '-49px', marginLeft: '50px'}}>
+          <Progress strokeLinecap="square" percent={75} status="active" showInfo={true}/>
+        </div>
+      </Tooltip>
     </div>
 
   )
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,47 +1,19 @@
-import {Avatar, Layout, Progress, Tooltip} from "antd";
-import React, {useContext, useEffect, useState} from "react";
+import {Layout} from "antd";
+import React from "react";
 import Account from "./Account";
-import avatar from "../../assets/images/avatar.png";
 import "./header.scss"
-import {StoreContext} from "../../repositories/rootRepo";
 
 const Header = (props) => {
-
   const {
     // isMobile = false,
     children
   } = props;
-
-  const store = useContext(StoreContext);
-  const [name, setName] = useState(null);
-  const [credits, setCredits] = useState(null);
-
-  useEffect(() => {
-    setName(store.authRepo.user.name);
-    setCredits(store.authRepo.user.credits);
-  }, []);
 
   return <Layout.Header style={{background: '#fff'}}>
     {/*<div className={'left-content'}>*/}
     {/*  <Brand collapsed={isMobile}/>*/}
     {/*</div>*/}
     {children}
-    <div className={'left-content'} style={{float: 'left', width: '175px'}}>
-      <Avatar
-        size={"large"}
-        className={'user-avatar'}
-        alt="avatar"
-        src={avatar}
-      />
-      <Tooltip title={'Credits: '+ credits}>
-        <span style={{paddingLeft: '10px'}}>
-          {name}
-        </span>
-        <div style={{height: '20px', marginTop: '-49px', marginLeft: '50px'}}>
-          <Progress strokeLinecap="square" percent={75} status="active" showInfo={true}/>
-        </div>
-      </Tooltip>
-    </div>
     <div className={'right-content'} style={{float: 'right'}}>
       <Account/>
     </div>
